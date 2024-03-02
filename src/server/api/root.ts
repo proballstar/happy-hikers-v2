e6f065cee6e0c5985b58b9887216b6214d072639
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2024-03-02 15:44:40 -0800

    set up nextjs and routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { listingsRouter } from "./routers/listings";
+import { usersRouter } from "./routers/user";
+import { blogsRouter } from "./routers/blogs";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,9 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  listings: listingsRouter,
+  user: usersRouter,
+  blog: blogsRouter
 });
 
 // export type definition of API
